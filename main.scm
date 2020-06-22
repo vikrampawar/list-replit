0bacf2d12d0eaea669036e2def9434c71956134c
vikrampawar <vikram.pawar@gmail.com>
2020-06-22 17:38:31 +0000

    Added sum-of-squares

--- main.scm
+++ main.scm
@@ -1,7 +1,18 @@
+;-------------------------------
+; shows how to define a variable
+(define pi 3.14)
+
+;-------------------------------
+; shows how to define a function
 (define (square x)
   (* x x))
-
 (square 5)
 
-; (define hello)
-; print(square)
+;-------------------------------
+; shows how to call another procedure
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+
+(sum-of-squares 6 8)
+;-------------------------------
+
